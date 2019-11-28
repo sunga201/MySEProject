dfc0c0346cf41ab105f0dec0b16387694230a9a2
sungs201 <sungs201@naver.com>
2019-11-28 21:10:38 +0900

    바뀐 내용 없음.

--- map_data.py
+++ map_data.py
@@ -2,12 +2,13 @@ import copy
 
 class MapData: #static class
     def __init__(self, mapSize=(20, 20), startSpot=(7, 9), objectSpot=[(17, 13), (14, 18), (10, 7), (16, 12), (19, 3), (5, 16), (15, 18), (8, 3), (0, 9), (7, 7), (5, 1), (8, 0), (3, 2), (3, 4), (1, 1), (0, 0)], hazardSpot=[(5, 18), (16, 7), (12, 8), (11, 17), (11, 9), (13, 5), (8, 1), (7, 5), (6, 9), (9, 9), (2, 3), (5, 4), (4, 4), (1, 0), (2, 4), (3, 1), (2, 0)]): #revise (Should remove initial data)
+    #def __init__(self, mapSize=(5, 0), startSpot=(3, 0), objectSpot=[(2, 0), (5, 0)], hazardSpot=([])):
     #def __init__(self, mapSize=(4, 5), startSpot=(1, 2), objectSpot=[(4, 2), (1, 5)], hazardSpot=([(1, 0), (3, 2)])):
         MapData.mapSize=mapSize
         MapData.startSpot=startSpot
         MapData.objectSpot=objectSpot
         MapData.hazardSpot=hazardSpot
-        MapData.hazardSpotH=[]
+        MapData.hazardSpotH = []
         MapData.colorBlobH=[]
 
     def setHiddenData(hazard, cb):
@@ -28,7 +29,6 @@ class MapData: #static class
 
     def getBackObjectSpot(list):
         MapData.objectSpot=copy.deepcopy(list)
-        print('restore : ', MapData.objectSpot)
         
     def getObjectSpot():
         return MapData.objectSpot

1e16942f568db3efa45aaa3485b20f0758cbb085
sungs201 <sungs201@naver.com>
2019-12-02 14:23:18 +0900

    no message

--- map_data.py
+++ map_data.py
@@ -3,7 +3,6 @@ import numpy as np
 
 class MapData: #static class
     #def __init__(self, mapSize=(20, 20), startSpot=(7, 9), objectSpot=[(17, 13), (14, 18), (10, 7), (16, 12), (19, 3), (5, 16), (15, 18), (8, 3), (0, 9), (7, 7), (5, 1), (8, 0), (3, 2), (3, 4), (1, 1), (0, 0)], hazardSpot=[(5, 18), (16, 7), (12, 8), (11, 17), (11, 9), (13, 5), (8, 1), (7, 5), (6, 9), (9, 9), (2, 3), (5, 4), (4, 4), (1, 0), (2, 4), (3, 1), (2, 0)]): #revise (Should remove initial data)
-    #def __init__(self, mapSize=(5, 0), startSpt=(3, 0), objectSpot=[(2, 0), (5, 0)], hazardSpot=([])):
     def __init__(self, mapSize=(4, 5), startSpot=(1, 2), objectSpot=[(4, 2), (1, 5)], hazardSpot=([(1, 0), (3, 2)])):
     #def __init__(self, mapSize, startSpot, objectSpot, hazardSpot):
         MapData.mapSize=mapSize
